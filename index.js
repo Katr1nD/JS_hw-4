c3c0668484f9ce44be0457c49adc78d14788bc31
Kart1nD <kathrindrako@gmail.com>
2024-10-07 22:20:54 +0300

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const value = 5;
+
+const double = (value) =>
+    new Promise((resolve) => resolve(value * 2));
+
+const addTen = (value) =>
+    new Promise((resolve) => resolve(value + 10));
+
+
+double(value)
+    .then(addTen)
+    .then((result) => {
+        console.log(result);
+    });
+
+
+
